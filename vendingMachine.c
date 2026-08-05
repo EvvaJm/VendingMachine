8c429b96e5395cfe18f7927a4195c4985031e0d8
EvvaJm <evva.morrissey@gmail.com>
2026-08-04 23:58:09 -0400

    Menu Display
    Added a menu display for vending machine options.

--- vendingMachine.c
+++ vendingMachine.c
@@ -1,1 +1,12 @@
 
+#include <stdio.h>
+
+int main (void){
+
+// this section prints out the menu for the user 
+printf("*** Please Make A Selection***\n");
+printf("1. Chips - $2\n2. Chocolate Bar - $1.50\n3. Pop - $1.25\n4. Water - $1\n");
+printf(" Enter your choice: ");
+  
+  return 0;
+}
