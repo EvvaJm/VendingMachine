012f85bb43fbd5fd219266b95262417a4c9c1d19
EvvaJm <evva.morrissey@gmail.com>
2026-08-10 21:06:25 -0400

    Implement input validation and price setting
    Added input validation and price assignment for vending machine selections.

--- vendingMachine.c
+++ vendingMachine.c
@@ -4,17 +4,29 @@
 int main (void){
 
 // variables 
-int chips = 200;
-int chocolateBar = 150;
-int pop = 125;
-int water = 100;
 int choice;
+int price; 
   
 // this section prints out the menu for the user 
 printf("*** Please Make A Selection***\n");
 printf("1. Chips - $2\n2. Chocolate Bar - $1.50\n3. Pop - $1.25\n4. Water - $1\n");
 printf(" Enter your choice: ");
 scanf("%d", &choice);
-printf("%d", choice);
-  return 0;
+
+// Makes sure the input is valid
+while (choice < 1 || choice > 5){
+printf("ERROR: Invalid number\n");
+printf(" Enter your choice: ");
+scanf("%d", &choice);
 }
+
+// sets the price variable 
+if ( choice == 1){
+  price = 200;
+} else if (choice == 2) {
+  price = 150;
+} else if (choice == 3){
+  price = 125;
+} else {
+  price = 100;
+} 
